a4205c68a2ba0e31ef711f8727def6a673d3890b
Peter Braden (HOPR) <peter.braden@hoprnet.org>
2020-08-18 11:34:36 +0200

    test ping

--- commands/commands.spec.ts
+++ commands/commands.spec.ts
@@ -15,5 +15,17 @@ describe('Commands', () => {
     expect(cmds.crawl).toBeTruthy()
   })
 
+  it('ping', async () => {
+    let mockNode: any = jest.fn()
+    mockNode.bootstrapServers = []
+    mockNode.ping = jest.fn()
+    let mockPeerId = '16Uiu2HAkyXRaL7fKu4qcjaKuo4WXizrpK63Ltd6kG2tH6oSV58AW'
+
+    let cmds = new mod.Commands(mockNode)
+    await cmds.ping.execute(mockPeerId)
+    expect(mockNode.ping).toHaveBeenCalled()
+
+  })
+
 })
 
